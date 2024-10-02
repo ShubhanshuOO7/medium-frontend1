648e583ad00fd15acd0fe0e7b22a03c60819722c
ShubhanshuOO7 <142130949+ShubhanshuOO7@users.noreply.github.com>
2024-10-02 21:37:23 +0530

    Add files via upload

---
+++ src/components/Quote.tsx
@@ -0,0 +1,20 @@
+
+export const Quote = () => {
+    return <div>
+        <div className="bg-slate-200 h-screen flex items-center flex-col justify-center">
+
+            <div className="max-w-md font-bold text-2xl">
+                "The customer service I recieved was exceptional.The support team went above and beyond to address my concerns"
+            </div>
+            <div className="self-start ml-28 mt-4" >
+                <div className="font-bold">
+                    Jules Winnfield
+                </div>
+                <div className="font-semibold text-slate-400 ">
+                    CEO, Acme inc
+                </div>
+
+            </div>
+        </div>
+    </div>
+}
